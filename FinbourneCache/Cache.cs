89359a643a91b599e44d27d7b9aeb4f0104237a0
Mitchell Hedges <mitchhedges@gmail.com>
2022-11-08 20:04:17 +0000

    Barebones .sln with a cache class that can be created

---
+++ FinbourneCache/Cache.cs
@@ -0,0 +1,15 @@
+﻿namespace FinbourneCache
+{
+    public class Cache<TKey, TValue> : ICache<TKey, TValue> where TKey : IComparable, IComparable<TKey>
+    {
+        public void Add(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue Get(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinbourneCache/ICache.cs
@@ -0,0 +1,8 @@
+﻿namespace FinbourneCache
+{
+    public interface ICache<TKey, TValue>
+    {
+        public void Add(TKey key, TValue value);
+        public TValue Get(TKey key);
+    }
+}
